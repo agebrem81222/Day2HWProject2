7bfa8465ee530cb506b55f4aad2c5406835b6464
alema <alema@10.0.0.114>
2023-04-08 14:36:12 -0400

    Initial commit

--- src/day2git/Day2HW2.java
+++ src/day2git/Day2HW2.java
@@ -5,5 +5,11 @@ public class Day2HW2 {
 	public static void main(String[] args) {
 		System.out.println( "day 2 HW2");
 	}
-
+public void divid(int a, int b) {
+	System.out.println(a/b);
+	
+}
+public void divid2(int a, int b) {
+	System.out.println(a/b);
+}
 }
